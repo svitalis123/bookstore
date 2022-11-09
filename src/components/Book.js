4896ad01da2915bc161b11da876664702e83c030
unknown <you@example.com>
2022-11-09 16:07:00 +0300

    Asses api for books

--- src/components/Book.js
+++ src/components/Book.js
@@ -7,10 +7,10 @@ const Book = (props) => {
   return (
     <div>
       {props.bookData.map((item) => (
-        <div key={item.id}>
-          <h2>{item.title}</h2>
-          <h3>{item.author}</h3>
-          <button type="button" onClick={() => dispatch(removebook(item.id))}>Remove</button>
+        <div key={item[0]}>
+          <h2>{item[1][0].title}</h2>
+          <h3>{item[1][0].author}</h3>
+          <button type="button" onClick={() => dispatch(removebook(item[0]))}>Remove</button>
         </div>
       ))}
     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -7,12 +7,14 @@ const Form = () => {
   const dispatch = useDispatch();
   const [title, setTitle] = useState();
   const [author, setAuthor] = useState();
+  const [category, setCategory] = useState('Leadership');
   const handleSubmit = (e) => {
     e.preventDefault();
     const obj = {
       id: uuidv4(),
       title,
       author,
+      category,
     };
     dispatch(addbook(obj));
     document.getElementById('form').reset();
@@ -21,6 +23,16 @@ const Form = () => {
     <form onSubmit={handleSubmit} id="form">
       <input type="text" onChange={(e) => setTitle(e.target.value)} name="title" placeholder="Book Title" required />
       <input type="text" onChange={(e) => setAuthor(e.target.value)} placeholder="Author" required />
+      <select
+        type="text"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+        placeholder="category"
+      >
+        <option value="Leadership">Leadership</option>
+        <option value="Science">Science</option>
+        <option value="Fiction">Fiction</option>
+      </select>
       <button type="submit">Add Book</button>
     </form>
   );
